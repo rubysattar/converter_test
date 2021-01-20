20e414aa9bd60433d415715219a46c40b40bb16e
rubysattar <rubysattar@gmail.com>
2021-01-20 16:30:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Independent practice using Mocha with Chai assertion library in JS
+I started this repository in order to practice running asynchronous code and get into the habit of test-driven-development.
+
+## Directions to follow along
+1. clone this repo to your local computer
+2. `cd` into the directory
+2. run the Web API with `node app/server.js`
+3. run `npm test`
---
+++ app/server.js
@@ -0,0 +1,24 @@
+// Implementation web server for the server test file
+const express = require("express")
+const app = express()
+const converter = require("./converter")
+
+app.get("/rgbToHex", function(req, res) {
+
+  const red   = parseInt(req.query.red, 10)
+  const green = parseInt(req.query.green, 10)
+  const blue  = parseInt(req.query.blue, 10)
+  const hex = converter.rgbToHex(red, green, blue)
+  res.send(hex)
+
+})
+
+app.get("/hexToRgb", function(req, res) {
+
+  const hex = req.query.hex
+  const rgb = converter.hexToRgb(hex)
+  res.send(JSON.stringify(rgb))
+
+})
+
+app.listen(3000)
